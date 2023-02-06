42af3ff833bb06d898948cf16441cd4faf4775c6
Arhangelov <arhangelov93@gmail.com>
2023-02-06 19:50:59 +0200

    updated services

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,39 +1,34 @@
-const url = 'https://expenses-tracker-production.up.railway.app/auth';
+const url = "https://expenses-tracker-arhangelov.vercel.app/auth";
 
 const getUser = async (username, email) => {
-    return fetch ( url + `/getUser`, {
-        method: 'POST',
+    return fetch(url + `/getUser`, {
+        method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email }),
-    }).then(res => res.json())
-}
+    }).then((res) => res.json());
+};
 
-const registerService = async ( email, username, password, confirmPassword ) => {
-    return fetch ( url + '/register' , {
+const registerService = async (email, username, password, confirmPassword) => {
+    return fetch(url + "/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, username, password, confirmPassword }),
-    }).then(res => res.json())
+    }).then((res) => res.json());
 };
 
-const loginService = async ( email, password ) => {
-    return fetch(url + '/login', {
+const loginService = async (email, password) => {
+    return fetch(url + "/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
-    }).then(res => res.json())
+    }).then((res) => res.json());
 };
 
-const logoutService = async ( ) => {
-    return fetch(url + '/logout', {
+const logoutService = async () => {
+    return fetch(url + "/logout", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
-    }).then(res => res.json())
+    }).then((res) => res.json());
 };
 
-export {
-    getUser,
-    registerService,
-    loginService,
-    logoutService
-}
+export { getUser, registerService, loginService, logoutService };
--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,32 +1,31 @@
-const url = 'https://expenses-tracker-production.up.railway.app/transaction';
+const url = "https://expenses-tracker-arhangelov.vercel.app/transaction";
 
-const getTransactionService = async ( username ) => {
-    return fetch ( url + '/get' , {
+const getTransactionService = async (username) => {
+    return fetch(url + "/get", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username }),
-    }).then(res => res.json())
+    }).then((res) => res.json());
 };
 
-const addTransactionService = async ( username, type, category, amount ) => {
-    return fetch ( url + '/add' , {
+const addTransactionService = async (username, type, category, amount) => {
+    return fetch(url + "/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, category, type, amount }),
-    }).then(res => res.json())
+    }).then((res) => res.json());
 };
 
-const deleteTransactionService = async ( id, username ) => {
-    return fetch ( url + `/delete/${id}` , {
+const deleteTransactionService = async (id, username) => {
+    return fetch(url + `/delete/${id}`, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username }),
-
-    }).then(res => res.json())
+    }).then((res) => res.json());
 };
 
 export {
     getTransactionService,
     addTransactionService,
-    deleteTransactionService
-}
+    deleteTransactionService,
+};
